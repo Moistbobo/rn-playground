cc494aed2eb82f26e20e53c208bc9e321fcfd842
Kevin Pang <kevin@appit.com.hk>
2021-09-22 04:15:04 +0800

    feat: Add drawer button to RootNavigator

--- src/navigation/RootNavigator/index.tsx
+++ src/navigation/RootNavigator/index.tsx
@@ -3,6 +3,7 @@ import Landing from 'features/Landing';
 import MobileStoreStack from 'navigation/RootNavigator/MobileStoreStack';
 import ReanimatedTabs from 'navigation/RootNavigator/ReanimatedTabs';
 import {createDrawerNavigator} from '@react-navigation/drawer';
+import DrawerButton from 'components/DrawerButton';
 
 export type RootNavigatorParamList = {
   Landing: undefined;
@@ -16,11 +17,16 @@ const RootNavigator = () => {
   return (
     <Drawer.Navigator
       screenOptions={{
-        headerShown: false,
+        headerLeftContainerStyle: {paddingLeft: 16},
+        headerLeft: () => <DrawerButton tintColor="black" />,
       }}
     >
       <Drawer.Screen name="Landing" component={Landing} />
-      <Drawer.Screen name="MobileStore" component={MobileStoreStack} />
+      <Drawer.Screen
+        options={{headerShown: false}}
+        name="MobileStore"
+        component={MobileStoreStack}
+      />
       <Drawer.Screen name="Reanimated" component={ReanimatedTabs} />
     </Drawer.Navigator>
   );
