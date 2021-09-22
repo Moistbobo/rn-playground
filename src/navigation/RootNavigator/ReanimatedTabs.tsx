0c66c5066a811c1142ac26f85eade816ea50792b
Kevin Pang <kevin@appit.com.hk>
2021-09-22 16:28:57 +0800

    refactor: remove safeArea from ReanimatedTabs navigator

--- src/navigation/RootNavigator/ReanimatedTabs.tsx
+++ src/navigation/RootNavigator/ReanimatedTabs.tsx
@@ -1,36 +1,39 @@
 import React from 'react';
+import {StyleSheet, ActivityIndicator, View} from 'react-native';
 import PanningScrollWithReactiveBackground from 'features/Reanimated/pages/PanningScrollWithReactiveBackground';
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
-import {ActivityIndicator, View} from 'react-native';
-import {SafeAreaView} from 'react-native-safe-area-context';
 import SpotifyLikePage from 'features/Reanimated/pages/SpotifyLikePage';
 
 const Tabs = createMaterialTopTabNavigator();
 
 const ReanimatedTabs = () => {
   return (
-    <SafeAreaView style={{flex: 1, backgroundColor: 'white'}} edges={['top']}>
-      <Tabs.Navigator
-        screenOptions={{
-          lazy: true,
-          lazyPlaceholder: () => (
-            <View
-              style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}
-            >
-              <ActivityIndicator size={24} color="blue" />
-            </View>
-          ),
-        }}
-      >
-        <Tabs.Screen
-          name="ReactiveScrollview"
-          component={PanningScrollWithReactiveBackground}
-        />
+    <Tabs.Navigator
+      screenOptions={{
+        lazy: true,
+        lazyPlaceholder: () => (
+          <View style={styles.placeholderContainer}>
+            <ActivityIndicator size={24} color="blue" />
+          </View>
+        ),
+      }}
+    >
+      <Tabs.Screen
+        name="ReactiveScrollview"
+        component={PanningScrollWithReactiveBackground}
+      />
 
-        <Tabs.Screen name="SpotifyLikePage" component={SpotifyLikePage} />
-      </Tabs.Navigator>
-    </SafeAreaView>
+      <Tabs.Screen name="SpotifyLikePage" component={SpotifyLikePage} />
+    </Tabs.Navigator>
   );
 };
+
+const styles = StyleSheet.create({
+  placeholderContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default ReanimatedTabs;
