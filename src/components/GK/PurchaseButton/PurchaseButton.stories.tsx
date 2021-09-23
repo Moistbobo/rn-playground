c96c436a93c83a2dd1050af7709f25f7b99911a7
Kevin Pang <kevin@appit.com.hk>
2021-09-23 23:35:55 +0800

    chore: remove unused purchase button component

--- src/components/GK/PurchaseButton/PurchaseButton.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import CenterView from 'storybook/stories/CenterView';
-import PurchaseButton from 'components/GK/PurchaseButton/index';
-import {action} from '@storybook/addon-actions';
-import {storiesOf} from '@storybook/react-native';
-
-storiesOf('components/GK', module)
-  .addDecorator(getStories => <CenterView>{getStories()}</CenterView>)
-  .add('PurchaseButton', () => <PurchaseButton onPress={action('onPress')} />);
