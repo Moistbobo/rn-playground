6c1e44387c3aedff4aa0111d26bf78470ac4fcb5
moistbobo <moistbobo@outlook.com>
2021-10-01 04:51:19 +0800

    refactor(SpotifyLikePage): Replace Animated.ScrollView with an animated FlatList

--- src/features/Reanimated/pages/SpotifyLikePage/index.tsx
+++ src/features/Reanimated/pages/SpotifyLikePage/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Animated from 'react-native-reanimated';
-import {Dimensions, StatusBar, StyleSheet, View} from 'react-native';
+import {Dimensions, FlatList, StatusBar, StyleSheet, View} from 'react-native';
 import {dollarSign, mdr1} from 'assets/images';
 import Spacer from 'components/Spacer';
 import ActionButtonsRow from 'components/GK/ActionButtonsRow';
@@ -26,6 +26,8 @@ const {height} = Dimensions.get('window');
 // dummy space so the scrollview can be scrolled enough to trigger animations */
 const DUMMY_SPACE = height * 0.45;
 
+const AnimatedFlatList = Animated.createAnimatedComponent(FlatList);
+
 /**
  * A page that imitates the spotify album page's scroll effect
  */
@@ -40,8 +42,23 @@ const SpotifyLikePage = () => {
 
   const {top: topSafeAreaInset} = useSafeAreaInsets();
 
+  const renderItem = ({item}: any) => (
+    <View style={styles.innerContentWrapper}>
+      <GKListItem
+        onPressItem={() => {
+          showFlashMessage(item.label);
+        }}
+        onPressMore={() => {
+          showFlashMessage(`${item.label}-more`);
+        }}
+        label={item.label}
+        subtitle={item.subtitle}
+      />
+    </View>
+  );
+
   return (
-    <View style={styles.container}>
+    <>
       <StatusBar
         translucent
         backgroundColor="transparent"
@@ -54,78 +71,83 @@ const SpotifyLikePage = () => {
       />
 
       <Animated.Image source={mdr1} style={[animatedImageStyle]} />
-
-      <Animated.ScrollView
+      <AnimatedFlatList
+        style={styles.container}
         onScroll={scrollHandler}
         scrollEventThrottle={16}
         showsVerticalScrollIndicator={false}
         // disable overscroll effect on android 12
         overScrollMode="never"
-      >
-        <Backdrop />
-
-        <View style={styles.innerContentWrapper}>
-          <Spacer size={imageSize + topSafeAreaInset} orientation="vertical" />
-
-          <GKCharaDetails
-            name="MDR"
-            makeNumber="No. 215"
-            description="Desert Tech ● Micro Dynamic Rifle"
-          />
-
-          <ActionButtonsRow
-            onPressLike={() => {
-              showFlashMessage('like');
-            }}
-            onPressDl={() => {
-              showFlashMessage('download');
-            }}
-            onPressMore={() => {
-              showFlashMessage('more');
-            }}
-          />
-
-          <Spacer size={16} />
-
-          {costumeList.map(item => (
+        ListHeaderComponent={() => {
+          return (
             <>
-              <GKListItem
-                onPressItem={() => {
-                  showFlashMessage(item.label);
-                }}
-                onPressMore={() => {
-                  showFlashMessage(`${item.label}-more`);
-                }}
-                label={item.label}
-                subtitle={item.subtitle}
-              />
-              <Spacer size={16} />
+              <Backdrop />
+
+              <View style={styles.innerContentWrapper}>
+                <Spacer
+                  size={imageSize + topSafeAreaInset}
+                  orientation="vertical"
+                />
+
+                <GKCharaDetails
+                  name="MDR"
+                  makeNumber="No. 215"
+                  description="Desert Tech ● Micro Dynamic Rifle"
+                />
+
+                <ActionButtonsRow
+                  onPressLike={() => {
+                    showFlashMessage('like');
+                  }}
+                  onPressDl={() => {
+                    showFlashMessage('download');
+                  }}
+                  onPressMore={() => {
+                    showFlashMessage('more');
+                  }}
+                />
+
+                <Spacer size={16} />
+              </View>
             </>
-          ))}
+          );
+        }}
+        ListFooterComponent={() => {
+          return (
+            <>
+              <Spacer size={16} />
+              <View style={styles.innerContentWrapper}>
+                <GKListSummary
+                  date={new Date().toISOString()}
+                  line2="2 Costumes"
+                />
+                <Spacer size={16} />
 
-          <GKListSummary date={new Date().toISOString()} line2="2 Costumes" />
-          <Spacer size={16} />
+                <GKRow label="Griffin & Kryuger PMC" iconSize={50} />
+                <Spacer size={16} />
+              </View>
 
-          <GKRow label="Griffin & Kryuger PMC" iconSize={50} />
-          <Spacer size={16} />
-        </View>
+              <GKAlsoLikeList
+                onItemPress={_item => () => {
+                  showFlashMessage(_item.label);
+                }}
+                label="You may also like"
+                data={alsoLike}
+              />
+              <Spacer size={32} />
 
-        <GKAlsoLikeList
-          onItemPress={_item => () => {
-            showFlashMessage(_item.label);
-          }}
-          label="You may also like"
-          data={alsoLike}
-        />
-        <Spacer size={32} />
+              <GKText variant="subtitle">
+                © SUNBORN Network Technology Co., Ltd. All Rights Reserved.
+              </GKText>
 
-        <GKText variant="subtitle">
-          © SUNBORN Network Technology Co., Ltd. All Rights Reserved.
-        </GKText>
-
-        <Spacer size={DUMMY_SPACE} />
-      </Animated.ScrollView>
-
+              <Spacer size={DUMMY_SPACE} />
+            </>
+          );
+        }}
+        ItemSeparatorComponent={() => <Spacer size={16} />}
+        data={costumeList}
+        renderItem={renderItem}
+      />
       <GKAnimatedButton
         animatedStyle={animatedButtonStyle}
         onPress={() => {
@@ -133,7 +155,7 @@ const SpotifyLikePage = () => {
         }}
         buttonImage={dollarSign}
       />
-    </View>
+    </>
   );
 };
 
